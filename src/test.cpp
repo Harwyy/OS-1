df8d2266f60d5544f57619b51569f5e7322fbba4
Harwyy <my-apple-good@mail.ru>
2024-11-10 19:31:57 +0300

    first-commit

---
+++ src/test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+// dasdasssssssssssssssssa dasdas das das dasd as dasdasdasssssssssssssssssa
+// dasdas das das dasd as dasdasdasssssssssssssssssa dasdas das das dasd
+// asdasdasdasssssssssssssssssa dasdas das das dasd as
+// dasdasdasssssssssssssssssadasdas das das dasd as dasdasdasssssssssssssssssa
+// dasdas das das dasd asdasdasdasssssssssssssssssa dasdas das das dasd as das
+int main() { 
+    int a = 10;
+    if (a == 10){
+        cout << "Hello " << endl;
+    }
+    return 1;
+ }
